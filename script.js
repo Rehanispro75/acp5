b29843844826b6a6d16be5d7dd1150029be8e49a
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-10-02 13:58:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var num1 =5;
+var num2 =5;
+var add = num1+num2;
+document.write(add);
+var num3 =5;
+var num4 =10;
+var substraction = num3-num4;
+document.write("<br>");
+document.write(substraction);
+var num5 =5;
+var num6 =5;
+var multiply =num5*num6
+document.write("<br>");
+document.write(multiply)
